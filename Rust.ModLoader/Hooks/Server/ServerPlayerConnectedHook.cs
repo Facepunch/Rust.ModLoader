2a2483df60f0b9d07bc4c5fd20ed4b5d045633f6
jamesalbert <jamesrobertalbert@gmail.com>
2021-01-11 20:26:01 -0800

    Add PlayerInit Hook

---
+++ Rust.ModLoader/Hooks/Server/ServerPlayerConnectedHook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+
+namespace Rust.ModLoader.Hooks.Server
+{
+    [HarmonyPatch(typeof(BasePlayer), "PlayerInit")]
+    public class ServerPlayerConnectedHook
+    {
+        [HarmonyPostfix]
+        public static void Postfix(BasePlayer __instance)
+        {
+            try
+            {
+                ModLoader.Scripts?.Broadcast("PlayerConnected", __instance);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+    }
+}
